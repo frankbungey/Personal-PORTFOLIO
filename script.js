0d3dbf24c0015c26808a9e41a1eaa775d9ce92e7
frankbungey <frankbungey@gmail.com>
2024-03-03 16:31:45 +0300

    almost done

--- script.js
+++ script.js
@@ -15,25 +15,25 @@ contactBtn.addEventListener('click', () => {
 // gallery effects
 const myGal1 = document.querySelector("#gallery1");
 
-function closeGallery1 (){
+function closeGallery1() {
     myGal1.style.gridColumn = "span 1"
     myGal1.style.height = "200px"
-    
+
 }
-function closeGallery2 (){
+function closeGallery2() {
     myGal2.style.gridColumn = "span 1"
     myGal2.style.height = "200px"
-    
+
 }
-function closeGallery3 (){
+function closeGallery3() {
     myGal3.style.gridColumn = "span 1"
     myGal3.style.height = "200px"
-    
+
 }
-function closeGallery4 (){
+function closeGallery4() {
     myGal4.style.gridColumn = "span 1"
     myGal4.style.height = "200px"
-    
+
 }
 
 myGal1.style.gridColumn = "span 1"
@@ -41,34 +41,36 @@ myGal1.style.height = "200px"
 
 myGal1.addEventListener('click', () => {
 
-if(myGal1.style.gridColumn == "span 1" && myGal1.style.height=="200px" ){
+    if (myGal1.style.gridColumn == "span 1" && myGal1.style.height == "200px") {
         myGal1.style.gridColumn = "span 5"
         myGal1.style.height = "500px"
         closeGallery2();
         closeGallery3();
         closeGallery4();
-    }else{
+        galInnerContent.style.display = "block";
+    } else {
         myGal1.style.height = "200px"
         myGal1.style.gridColumn = "span 1"
+        galInnerContent.style.display = "none";
     }
-    
+
 });
 const myGal2 = document.querySelector("#gallery2");
 
 myGal2.style.gridColumn = "span 1"
 myGal2.style.height = "200px"
 
-myGal2.addEventListener("click",()=>{
-    if(myGal2.style.gridColumn == "span 1" && myGal2.style.height=="200px" ){
+myGal2.addEventListener("click", () => {
+    if (myGal2.style.gridColumn == "span 1" && myGal2.style.height == "200px") {
         myGal2.style.gridColumn = "span 5"
         myGal2.style.height = "500px"
         closeGallery1();
         closeGallery3();
         closeGallery4();
-    }else{
+    } else {
         myGal2.style.height = "200px"
         myGal2.style.gridColumn = "span 1"
-       
+
     }
 })
 
@@ -77,17 +79,17 @@ const myGal3 = document.querySelector("#gallery3");
 myGal3.style.gridColumn = "span 1"
 myGal3.style.height = "200px"
 
-myGal3.addEventListener("click",()=>{
-    if(myGal3.style.gridColumn == "span 1" && myGal3.style.height=="200px" ){
+myGal3.addEventListener("click", () => {
+    if (myGal3.style.gridColumn == "span 1" && myGal3.style.height == "200px") {
         myGal3.style.gridColumn = "span 5"
         myGal3.style.height = "500px"
         closeGallery1();
         closeGallery2();
         closeGallery4();
-    }else{
+    } else {
         myGal3.style.height = "200px"
         myGal3.style.gridColumn = "span 1"
-       
+
     }
 })
 
@@ -96,18 +98,65 @@ const myGal4 = document.querySelector("#gallery4");
 myGal4.style.gridColumn = "span 1"
 myGal4.style.height = "200px"
 
-myGal4.addEventListener("click",()=>{
-    if(myGal4.style.gridColumn == "span 1" && myGal4.style.height=="200px" ){
+myGal4.addEventListener("click", () => {
+    if (myGal4.style.gridColumn == "span 1" && myGal4.style.height == "200px") {
         myGal4.style.gridColumn = "span 5"
         myGal4.style.height = "500px"
         closeGallery1();
         closeGallery2();
         closeGallery3();
-    }else{
+    } else {
         myGal4.style.height = "200px"
         myGal4.style.gridColumn = "span 1"
-       
+
     }
 })
 
+const galInnerContent = document.createElement('div');
+galInnerContent.classList.add('.gal1-inner-cont');
 
+const h1Tag = document.createElement('h1');
+const pTag = document.createElement('p');
+
+galInnerContent.appendChild(h1Tag);
+galInnerContent.appendChild(pTag);
+myGal1.appendChild(galInnerContent);
+
+// message
+const messageBtn = document.getElementById("messagebtn");
+const myName = document.getElementById("name");
+const myEmail = document.getElementById("email");
+const textArea = document.querySelector(".text");
+
+messageBtn.addEventListener("click", () => {
+if(myName.value == ""){
+    throwErrorName();
+    alert('Inputs Cant Be Empty')
+}
+else if(myEmail.value == ""){
+    throwErrorEmail();
+}
+else if(textArea.value == ""){
+    throwErrorText();
+}
+else{
+    alert("Thank You...")
+}
+myName.value = "";
+myEmail.value = "";
+textArea.value = "";
+});
+
+function throwErrorName() {
+    myName.innerText = "Kindly Provide Your Nmae"
+    myName.style.color = "red"
+    myName.style.border = "1px solid red"
+}
+function throwErrorEmail() {
+    myEmail.style.color = "red"
+    myEmail.style.border = "1px solid red"
+}
+function throwErrorText() {
+   textArea.style.color = "red"
+   textArea.style.border = "1px solid red"
+}
